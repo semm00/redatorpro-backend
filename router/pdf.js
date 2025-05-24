358d7e5b017c7fd32df692500e63ade9df9533c0
semm00 <sofiasemmemily@gmail.com>
2025-05-24 01:22:47 -0300

    scr

--- router/pdf.js
+++ router/pdf.js
@@ -96,19 +96,16 @@ router.post("/gerar-pdf", async (req, res) => {
         let linhasTexto = linhasTextareaParaPDF(texto, totalLinhas);
 
         // Desenha o texto, linha a linha, igual ao textarea
-       
         for (let i = 0; i < linhasTexto.length && i < totalLinhas; i++) {
-        const y = pageHeight - marginTop - i * lineSpacing + 5;
-        const larguraLinha = fonte.widthOfTextAtSize(linhasTexto[i], tamanhoFonte);
-        const x = marginX + ((maxWidth - larguraLinha) / 2); // Centraliza horizontalmente
-        page.drawText(linhasTexto[i], {
-            x,
-            y,
-            size: tamanhoFonte,
-            font: fonte,
-            color: preto,
-        });
-    }
+            const y = pageHeight - marginTop - i * lineSpacing + 5;
+            page.drawText(linhasTexto[i], {
+                x: marginX + 3,
+                y,
+                size: tamanhoFonte,
+                font: fonte,
+                color: preto,
+            });
+        }
 
         // Logo centralizada e com proporção melhor
         const logoPath = path.join(__dirname, "public/logo nome.png");
